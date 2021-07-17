56f99f13d477e02bcc7fb378b0c876207e4b17a7
Filip Dobrovolny <dobrovolny.filip@gmail.com>
2021-07-17 16:04:42 +0200

    Adds missing ULID requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["pynamodb==5.1.0"]
+requirements = ["pynamodb==5.1.0", "ulid-py"]
 
 setup_requirements = [
     "pytest-runner",
